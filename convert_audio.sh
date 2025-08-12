21887935cdb8eed445156dabd7862e825c15c5fc
isaiahdoyle <isaiahdoyle56@gmail.com>
2025-08-12 16:31:22 -0700

    generalize convert_audio.sh

--- convert_audio.sh
+++ convert_audio.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-for filename in audio/*.mp3; do
-    ffmpeg -i "$filename" -ar 24000 "audio/converted/$(basename "$filename" .mp3).wav"
+
+# Usage: `bash convert_audio.sh <inext> <outext> <outdir>`
+#   -> will convert all files in working directory of type <inext> to type
+#      <outext> and place results in <outdir>
+
+for filename in ./*."$1"; do
+    ffmpeg -i "$filename" -ar 24000 "$3/$(basename "$filename" .$1).$2"
 done
